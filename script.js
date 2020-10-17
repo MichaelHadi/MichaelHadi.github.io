152ad39e949e58c0429fbff9a7ad60df6d407c9d
michaelhadisaputra <cryworld231@gmail.com>
2020-10-17 20:02:08 +0700

    s

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function function1(){
   var d = document.getElementById("reg4").value;
   var e = document.getElementById("reg5").value;
   var f = document.getElementById("reg6").value;
+  if (a != "" && b != "" && c != "" && d != "" && e != "" && f != "" ){
   alert ("Submitted\n" + a + " "+ b + "\n" + c + "\n" + d + "\n" + e + "\n" + f);
+  }
 }
 function function2(){
   var a = document.getElementById("con1").value;
-  alert ("Thank you for your question " + a +"\n"+"Wait for our response in your email");
+  var b = document.getElementById("con2").value;
+  var c = document.getElementById("con3").value;
+  if (a != "" && b != "" && c != "" ){
+    alert ("Thank you for your question " + a +"\n"+"Wait for our response in your email");
+  }
 }
 function myFunction() {
   var dots = document.getElementById("dots");
